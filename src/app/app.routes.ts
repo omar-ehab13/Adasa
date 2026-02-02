a258353e9c9d91bd579957df9347cfdf072e732e
omar-ehab13 <omarehab1308@gmail.com>
2026-02-02 07:10:01 +0200

    Adding routing and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent, title: 'عدسة عالم التصوير' },
+    { path: 'blog', component: BlogComponent, title: 'عدسة عالم التصوير' },
+    { path: 'about', component: AboutComponent, title: 'عدسة عالم التصوير' },
+    { path: '**', component: NotFoundComponent }
+];
