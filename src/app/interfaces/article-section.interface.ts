c9126f550cbcc6df03d49e44f03156b6de1f24a4
omar-ehab13 <omarehab1308@gmail.com>
2026-02-08 20:38:32 +0200

    fix sections and sidebar in blog-details page

---
+++ src/app/interfaces/article-section.interface.ts
@@ -0,0 +1,5 @@
+export interface IArticleSection {
+    id: string;
+    title: string;
+    content: string;
+}
--- src/app/pages/blog-details/blog-details.component.ts
+++ src/app/pages/blog-details/blog-details.component.ts
@@ -1,26 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { IPost } from '../../interfaces/post.interface';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PostService } from '../../services/post.service';
+import { IArticleSection } from '../../interfaces/article-section.interface';
 
 @Component({
   selector: 'app-blog-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './blog-details.component.html',
   styleUrl: './blog-details.component.css',
 })
 export class BlogDetailsComponent implements OnInit {
   post !: IPost | undefined;
   suggestPosts !: IPost[];
+  postService = inject(PostService)
 
-  constructor(private route: ActivatedRoute, private postService: PostService){
+  introText: string = '';
+  sections: IArticleSection[] = [];
 
-  }
+  constructor(private route: ActivatedRoute) { }
+
   ngOnInit(): void {
     const slug = this.route.snapshot.paramMap.get('slug');
     this.post = this.postService.getPostBySlug(slug!);
-    console.log(this.post);
-    if (this.post)
-      this.suggestPosts = this.postService.getRandomPosts(3, [this.post!.id]);
+
+    if (this.post) {
+      this.parseContent(this.post.content);
+
+      this.suggestPosts = this.postService.getRandomPosts(
+        3,
+        [this.post.id]
+      );
+    }
   }
+
+
+  private parseContent(content: string) {
+    // قسم المحتوى على أساس ## 
+    const parts = content.split('\n\n## ');
+
+    // أول جزء = المقدمة
+    this.introText = parts.shift() ?? '';
+
+    this.sections = parts.map((part, index) => {
+      const [title, ...body] = part.split('\n\n');
+
+      return {
+        id: `section-${index}`,
+        title: title.trim(),
+        content: body.join('\n\n').trim(),
+      };
+    });
+  }
+
 }
